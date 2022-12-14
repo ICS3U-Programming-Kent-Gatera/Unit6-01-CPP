8468ef73e04e1c967c9dbc9d02f23543444fe680
Kent Gatera <kent.gatera@stu.ocsb.ca>
2022-12-14 08:11:52 +0000

    Average calculator

--- averageCalc.cpp
+++ averageCalc.cpp
@@ -7,7 +7,6 @@
 #include <iostream>
 #include <random>
 #include <string>
-using namespace std;
 
 int main() {
     // Declaring all the variables and constants.
@@ -15,16 +14,16 @@ int main() {
     const int MIN_NUM = 0;
     const int MAX_NUM = 100;
     int arrayOfInt[MAX_ARRAY_SIZE];
-    string arrayOfStr[MAX_ARRAY_SIZE];
+    std::string arrayOfStr[MAX_ARRAY_SIZE];
     int someRandomNumber;
     float sumNum;
     float ansNum;
 
     for (int i = 0; i < MAX_ARRAY_SIZE; i++) {
         // Generating the random number.
-        random_device rseed;
-        mt19937 rgen(rseed());
-        uniform_int_distribution<int> idist(MIN_NUM, MAX_NUM);
+        std::random_device rseed;
+        std::mt19937 rgen(rseed());
+        std::uniform_int_distribution<int> idist(MIN_NUM, MAX_NUM);
         someRandomNumber = idist(rgen);
 
         // Adding the random number to the list.
@@ -34,10 +33,10 @@ int main() {
     }
     // Dividing up all the numbers by the total/max numbers in the array.
     ansNum = sumNum / MAX_ARRAY_SIZE;
-    cout << "The average of [";
+    std::cout << "The average of [";
     for (int i = 0; i < MAX_ARRAY_SIZE - 1; i++) {
-        cout << arrayOfInt[i] << ", ";
+        std::cout << arrayOfInt[i] << ", ";
     }
     // 9 is the final number in the index since the array is 10 elements.
-    cout << arrayOfInt[9] << "] is " << ansNum << "\n";
+    std::cout << arrayOfInt[9] << "] is " << ansNum << "\n";
 }
